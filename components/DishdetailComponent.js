6940b2282daf0e42d865bd615c057e67e8199974
Gregmar Cruz <gregr1201@gmail.com>
2019-12-19 13:32:40 -0500

    Social Sharing

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet, FlatList, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, ModalFooter, Row, Col, Label, LocalForm, Control, Errors, Alert, PanResponder } from 'react-native';
+    CardTitle, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, ModalFooter, Row, Col, Label, LocalForm, Control, Errors, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input, Button } from 'react-native-elements';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
@@ -70,6 +70,16 @@ function RenderDish(props) {
         }
     })
 
+    const shareDish = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: title + ': ' + message + ' ' + url,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        })
+    }
+
         if (dish != null) {
             return (
                 <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
@@ -96,6 +106,14 @@ function RenderDish(props) {
                                         type='font-awesome'
                                         color='#f50'
                                         onPress={() => props.onSelect()} />
+                                    <Icon
+                                        raised
+                                        reverse
+                                        name='share'
+                                        type='font-awesome'
+                                        color='#51D2A8'
+                                        style={styles.cardItem}
+                                        onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)} />
                                 </View>
                     </Card>
                 </Animatable.View>
